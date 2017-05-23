94c55bb7aa2087c0bc07e4044582a688a1bb1598
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-05-23 16:42:38 +0100

    Note view list

--- view/note-list-view-model.js
+++ view/note-list-view-model.js
@@ -1,18 +1,4 @@
 (function(exports) {
-  function ViewList(noteList) {
-    this.noteList = noteList;
-  };
-  exports.ViewList = ViewList;
-})(this);
-
-ViewList.prototype.view = function() {
-  var html = "";
-  for (var i =0; i < this.noteList.array.length; i++) {
-        html += "<ul><li><div>" + this.noteList.array[i]+ "</ul></li></div>";
-      }; return html
-};
-
-(function(exports) {
   function testViewList() {
     var list = new List();
     list.add("ListOne");
--- view/note-list-view.js
+++ view/note-list-view.js
@@ -9,13 +9,5 @@ ViewList.prototype.view = function() {
   var html = "";
   for (var i =0; i < this.noteList.array.length; i++) {
         html += "<ul><li><div>" + this.noteList.array[i]+ "</ul></li></div>";
-        console.log(html.toString);
-      };
+      }; return html
 };
-
-list = new List();
-list.add("n1");
-list.add("n2");
-list.add("n3");
-vl = new ViewList();
-vl.noteList = list;
