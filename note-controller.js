d4b327abe00dda62e083354c3c440f6ae232b33a
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-05-24 14:39:51 +0100

    Changed HTML ID from controller

--- note-controller.js
+++ note-controller.js
@@ -1,1 +1,16 @@
-clkdfajfjer
+(function() {
+  document.getElementById("app").innerHTML = "Howdy!";
+console.log();
+console.log("STRING");
+})();
+
+
+// (function(exports) {
+//   function testNote() {
+//     var note = new Note();
+//
+//     assert.isTrue(note.text === "My favourite language is JavaScript");
+//   };
+//
+//   testNote();
+// })(this);
